0d255b135bf3f8692b5f716029b72e11614f5065
aaron gershenzon and jared beckler <jared.beckler@gmail.com>
2016-01-14 11:02:14 -0800

    add js for definitions and comment out possible unneeded info

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,58 @@
 $(document).ready(function() {
 
   $("h3#markdown").click(function(){
-    $("p.markdown").toggle();
-  });
-  $("h3#blockele").click(function(){
-    $("p.blockele").toggle();
+    $("p").removeClass("active");
+    $("p.markdown").addClass("active");
+    $("p.active").siblings().hide();
+    $("p.active").show();
   });
 
+  $("h3#looping").click(function(){
+    $("p").removeClass("active");
+    $("p.looping").addClass("active");
+    $("p.active").siblings().hide();
+    $("p.active").show();
+  });
+
+  $("h3#forms").click(function(){
+    $("p").removeClass("active");
+    $("p.forms").addClass("active");
+    $("p.active").siblings().hide();
+    $("p.active").show();
+  });
+
+  $("h3#css").click(function(){
+    $("p").removeClass("active");
+    $("p.css").addClass("active");
+    $("p.active").siblings().hide();
+    $("p.active").show();
+  });
+
+  $("h3#bootstrap").click(function(){
+    $("p").removeClass("active");
+    $("p.bootstrap").addClass("active");
+    $("p.active").siblings().hide();
+    $("p.active").show();
+  });
+
+  $("h3#divspan").click(function(){
+    $("p").removeClass("active");
+    $("p.divs").addClass("active");
+    $("p.active").siblings().hide();
+    $("p.active").show();
+  });
+
+  $("h3#floats").click(function(){
+    $("p").removeClass("active");
+    $("p.floats").addClass("active");
+    $("p.active").siblings().hide();
+    $("p.active").show();
+  });
+
+  $("h3#box").click(function(){
+    $("p").removeClass("active");
+    $("p.box").addClass("active");
+    $("p.active").siblings().hide();
+    $("p.active").show();
+  });
 });
