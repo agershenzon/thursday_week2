df49672e39da4d7fb2fedeec4ab3b9d13ff15066
aaron gershenzon and jared beckler <aarongershenzon@gmail.com>
2016-01-14 08:58:50 -0800

    add definition jumbotron and toggle effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
 
+  $("h3#markdown").click(function(){
+    $("p.markdown").toggle();
+  });
+  $("h3#blockele").click(function(){
+    $("p.blockele").toggle();
+  });
 
 });
