e0eb2bc03d26db0559811b244a18f205c942d2f8
aaron gershenzon and jared beckler <aarongershenzon@gmail.com>
2016-01-14 16:04:31 -0800

    add simplification to click functionin js file, adjust indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,14 @@
 $(document).ready(function() {
 
-  $("h3#markdown").click(function(){
-    $("span").removeClass("active");
-    $("span.markdown").addClass("active");
-    $("span").siblings().hide();
-    $("span.active").show();
-  });
 
-  $("h3#looping").click(function(){
+// D.R.Y. click function
+  $("h3.vocab").click(function(){
     $("span").removeClass("active");
-    $("span.looping").addClass("active");
+    $("span." + this.id).addClass("active");
     $("span.active").siblings().hide();
     $("span.active").show();
   });
-
-  $("h3#forms").click(function(){
-    $("span").removeClass("active");
-    $("span.forms").addClass("active");
-    $("span.active").siblings().hide();
-    $("span.active").show();
-  });
-
-  $("h3#css").click(function(){
-    $("span").removeClass("active");
-    $("span.css").addClass("active");
-    $("span.active").siblings().hide();
-    $("span.active").show();
-  });
-
-  $("h3#bootstrap").click(function(){
-    $("span").removeClass("active");
-    $("span.bootstrap").addClass("active");
-    $("span.active").siblings().hide();
-    $("span.active").show();
-  });
-
-  $("h3#divspan").click(function(){
-    $("span").removeClass("active");
-    $("span.divs").addClass("active");
-    $("span.active").siblings().hide();
-    $("span.active").show();
-  });
-
-  $("h3#floats").click(function(){
-    $("span").removeClass("active");
-    $("span.floats").addClass("active");
-    $("span.active").siblings().hide();
-    $("span.active").show();
-  });
-
-  $("h3#box").click(function(){
-    $("span").removeClass("active");
-    $("span.box").addClass("active");
-    $("span.active").siblings().hide();
-    $("span.active").show();
-  });
+// End
 
   $("#loopbtn").click(function() {
     var looping = ['I', 'Love', 'Loops!']
@@ -69,5 +23,64 @@ $(document).ready(function() {
     $("#formGreet").show();
     event.preventDefault();
   });
-
 });
+
+
+// Enlongated click function
+
+
+  // $("h3#markdown").click(function(){
+  //   $("span").removeClass("active");
+  //   $("span.markdown").addClass("active");
+  //   $("span").siblings().hide();
+  //   $("span.active").show();
+  // });
+  //
+  // $("h3#looping").click(function(){
+  //   $("span").removeClass("active");
+  //   $("span.looping").addClass("active");
+  //   $("span.active").siblings().hide();
+  //   $("span.active").show();
+  // });
+  //
+  // $("h3#forms").click(function(){
+  //   $("span").removeClass("active");
+  //   $("span.forms").addClass("active");
+  //   $("span.active").siblings().hide();
+  //   $("span.active").show();
+  // });
+  //
+  // $("h3#css").click(function(){
+  //   $("span").removeClass("active");
+  //   $("span.css").addClass("active");
+  //   $("span.active").siblings().hide();
+  //   $("span.active").show();
+  // });
+  //
+  // $("h3#bootstrap").click(function(){
+  //   $("span").removeClass("active");
+  //   $("span.bootstrap").addClass("active");
+  //   $("span.active").siblings().hide();
+  //   $("span.active").show();
+  // });
+  //
+  // $("h3#divspan").click(function(){
+  //   $("span").removeClass("active");
+  //   $("span.divs").addClass("active");
+  //   $("span.active").siblings().hide();
+  //   $("span.active").show();
+  // });
+  //
+  // $("h3#floats").click(function(){
+  //   $("span").removeClass("active");
+  //   $("span.floats").addClass("active");
+  //   $("span.active").siblings().hide();
+  //   $("span.active").show();
+  // });
+  //
+  // $("h3#box").click(function(){
+  //   $("span").removeClass("active");
+  //   $("span.box").addClass("active");
+  //   $("span.active").siblings().hide();
+  //   $("span.active").show();
+  // });
